d2164dc7e3104718d0839eca10a2b37b21774cbb
Joseph Harburg <josephharburg@gmail.com>
2019-02-10 13:12:09 -0500

    Publishing my website

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// const expandclick = 'poop';
+// const newdiv = '<div class="expandingBox"> Graphic Design </div>';
+// const divelement = document.querySelector('.containertwo')
+// const boxthere = document.querySelector('.expandingBox')
+// const divone = document.querySelector('.containertwo')
+// var isfalse = true
+// function expand (evt) {
+//   if (isfalse === true) {
+//     divelement.insertAdjacentHTML('beforebegin', newdiv);
+//     isfalse = false;
+//   }
+// }
+//
+//
+// divelement.addEventListener('click', expand)
