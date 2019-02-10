45d5642eb82c0a2a7d726421384c721dbcfdc839
Joseph Harburg <josephharburg@gmail.com>
2019-02-10 17:42:51 -0500

    Added menu button and about me page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-// const expandclick = 'poop';
-// const newdiv = '<div class="expandingBox"> Graphic Design </div>';
-// const divelement = document.querySelector('.containertwo')
-// const boxthere = document.querySelector('.expandingBox')
-// const divone = document.querySelector('.containertwo')
-// var isfalse = true
-// function expand (evt) {
-//   if (isfalse === true) {
-//     divelement.insertAdjacentHTML('beforebegin', newdiv);
-//     isfalse = false;
-//   }
-// }
-//
-//
-// divelement.addEventListener('click', expand)
+
+const dropDown = document.querySelector('.menuButton');
+const menu = "<div class='dropDown'><a href='pages/Aboutme.html'>About Me</a></div>";
+const findMenu = document.querySelector('.dropDown');
+var dropped = false;
+function expand (event) {
+  if (event.target === dropDown && dropped === false) {
+    dropDown.insertAdjacentHTML("afterend", menu);
+    return dropped = true;
+  }
+  else {
+    const findMenutwo = document.querySelector('.dropDown');
+    findMenutwo.parentNode.removeChild(findMenutwo)
+    return dropped = false;
+  }
+}
+
+dropDown.addEventListener('click', expand)
